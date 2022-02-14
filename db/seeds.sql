47a97838876e040635c15762a38fca9ca2759fce
Sajees XPS <sajees.jeya@gmail.com>
2022-02-14 01:02:27 -0500

    updated server

--- db/seeds.sql
+++ db/seeds.sql
@@ -8,11 +8,11 @@ INSERT INTO job (title, salary, department_id)
 VALUES
 ('Lead Engineer', 150000, 1), 
 ('Software Engineer', 120000, 1), 
-('Junior Developer', 100000, 1)
+('Junior Developer', 100000, 1),
 ('Account Manager', 160000, 2), 
-('Accountant', 125000, 2), 
-('Legal Team Lead', 250000, 3), 
-('Lawyer', 190000, 3); 
+('Accountant', 125000, 2),
+('Legal Team Lead', 250000, 3),
+('Lawyer', 190000, 3);
 
 INSERT INTO employee (first_name, last_name, job_id, manager_id)
 VALUES
